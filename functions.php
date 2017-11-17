08cefb8d96e9153bafdb7c7b5d58979e37eb6d69
Ned Zimmerman <ned@bight.ca>
2017-11-17 17:59:04 -0400

    Use Buckram as dependency.

--- functions.php
+++ functions.php
@@ -4,8 +4,10 @@
  * @license GPLv2 (or any later version)
  */
 
-function clarke_theme_setup() {
+add_action( 'after_setup_theme', function () {
 	add_theme_support( 'pressbooks_global_typography', 'grc' );
-}
+} );
 
-add_action( 'after_setup_theme', 'clarke_theme_setup' );
+add_filter( 'pb_global_components_path', function( $path ) {
+	return dirname( __FILE__ ) . '/node_modules/buckram/styles/';
+} );
