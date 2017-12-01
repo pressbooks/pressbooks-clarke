a2e54f929ceac041b539b2a2874bcffc8980537e
Ned Zimmerman <ned@bight.ca>
2017-12-01 13:41:20 -0400

    Cleanup.

--- functions.php
+++ functions.php
@@ -7,7 +7,3 @@
 add_action( 'after_setup_theme', function () {
 	add_theme_support( 'pressbooks_global_typography', 'grc' );
 } );
-
-add_filter( 'pb_global_components_path', function( $path ) {
-	return dirname( __FILE__ ) . '/node_modules/buckram/styles/';
-} );
